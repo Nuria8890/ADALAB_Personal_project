6373f7bfae9b9f3079202d9b3fccb0268023b863
Nuria Campo <nuria.campo7@gmail.com>
2024-12-06 20:06:01 +0100

    header feo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
 import "../styles/layout/Header.scss";
+import Carrito from "../images/carrito_fast.png";
+
 function Header() {
-  return <header className="header">HEADER</header>;
+  return (
+    <header className="header">
+      <img className="header__logo" src={Carrito} alt="carrito" />
+      <h1>Súper compras</h1>
+    </header>
+  );
 }
 
 export default Header;
